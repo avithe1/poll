66274fce54b84492c54959d903401b87bf95e1db
Avinash <avithe1@gmail.com>
2024-12-04 12:18:04 +0530

    somewhat worked

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,52 +1,55 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import { pollquestions } from "./utils";
 
-const pollquestions = [
-  {
-    id: "1",
-    question: "how is your day today?",
-    options: ["its ok", "I have seen better days", "dont ask me about it"],
-    polltaken: false,
-  },
-  {
-    id: "2",
-    question: "What person are you?",
-    options: ["Beach person", "Mountain person", "I dont care"],
-    polltaken: false,
-  },
-];
+// const pollquestions = [
+//   {
+//     id: "1",
+//     question: "how is your day today?",
+//     options: ["its ok", "I have seen better days", "dont ask me about it"],
+//     polltaken: false,
+//   },
+//   {
+//     id: "2",
+//     question: "What person are you?",
+//     options: ["Beach person", "Mountain person", "I dont care"],
+//     polltaken: false,
+//   },
+// ];
 
 function App() {
-  const [assignedPaths, setAssignedPaths] = useState({});
   const [currentPoll, setCurrentPoll] = useState(null);
   useEffect(() => {
     console.log("window.location.pathname : ", window.location.pathname);
     let thepath = window.location.pathname;
     if (localStorage.getItem(thepath)) {
-      if (assignedPaths[thepath]) {
-      } else {
-        assignedPaths[thepath] = (
-          Object.keys(assignedPaths).length + 1
-        ).toString();
-        setCurrentPoll(
-          pollquestions.filter(
-            (poll) =>
-              poll.id === (Object.keys(assignedPaths).length + 1).toString()
-          )[0]
-        );
-      }
-    } else {
-      assignedPaths[thepath] = (
-        Object.keys(assignedPaths).length + 1
-      ).toString();
+      console.log("found the path in localstorage : ", thepath);
       setCurrentPoll(
-        pollquestions.filter(
-          (poll) =>
-            poll.id === (Object.keys(assignedPaths).length + 1).toString()
-        )[0]
+        pollquestions[parseInt(localStorage.getItem(thepath)) - 1]
       );
+    } else {
+      let items = localStorage.getItem("assignedpaths");
+      if (items) {
+        console.log("assigned paths has items", items.split(","));
+        let assignedPaths = items.split(",");
+        let available = pollquestions.filter(
+          (obj) => !assignedPaths.includes(obj.id)
+        );
+        if (available.length) {
+          localStorage.setItem("assignedpaths", [
+            ...assignedPaths,
+            available[0].id,
+          ]);
+          localStorage.setItem(thepath, available[0].id);
+          setCurrentPoll(pollquestions[available[0].id - 1]);
+        }
+      } else {
+        console.log("assigned paths has NO items");
+        localStorage.setItem("assignedpaths", [1]);
+        localStorage.setItem(thepath, "1");
+        setCurrentPoll(pollquestions[0]);
+      }
     }
-    console.log("assignedPaths:", assignedPaths);
   }, [window.location.pathname]);
 
   return (
---
+++ my-app/src/utils.js
@@ -0,0 +1,15 @@
+export const pollquestions = [
+    {
+      id: "1",
+      question: "how is your day today?",
+      options: ["its ok", "I have seen better days", "dont ask me about it"],
+      polltaken: false,
+    },
+    {
+      id: "2",
+      question: "What person are you?",
+      options: ["Beach person", "Mountain person", "I dont care"],
+      polltaken: false,
+    },
+  ];
+  
